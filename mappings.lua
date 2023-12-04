dfe04883244823689bf1d2bbdff509f01175a68f
exoutia <jhav365@gmail.com>
2023-12-04 09:58:29 +0530

    remove old tmux configuration cause I am not using it right now.

--- mappings.lua
+++ mappings.lua
@@ -3,10 +3,6 @@ local M = {}
 
 M.general = {
   n = {
-    ["<C-h>"] = { "<cmd> TmuxNavigateLeft<CR>", "window left" },
-    ["<C-l>"] = { "<cmd> TmuxNavigateRight<CR>", "window right" },
-    ["<C-j>"] = { "<cmd> TmuxNavigateDown<CR>", "window down" },
-    ["<C-k>"] = { "<cmd> TmuxNavigateUp<CR>", "window up" },
     ["<leader>gn"] = { "<cmd> Neogit<CR>", "neogit open" },
     ["<leader>fp"] = { "<cmd> ProjectMgr<CR>", "Open Project Manager" },
     ["<C-i>"] = { "<cmd>lua require('notify').dismiss()<CR>", "dismiss notification" },
