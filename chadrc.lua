fe2999637f18b46df1382f7aa71953aee217d117
exoutia <jhav365@gmail.com>
2023-12-09 09:12:06 +0530

    including jdtls into lsp

--- chadrc.lua
+++ chadrc.lua
@@ -5,8 +5,8 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "catppuccin",
-  theme_toggle = { "catppuccin", "catppuccin" },
+  theme = "gruvchad",
+  theme_toggle = { "gruvchad", "gruvchad" },
 
   hl_override = highlights.override,
   hl_add = highlights.add,
--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -18,6 +18,7 @@ local servers = {
   "marksman",
   "gopls",
   "elixirls",
+  "jdtls",
 }
 
 for _, lsp in ipairs(servers) do
