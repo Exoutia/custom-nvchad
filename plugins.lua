9180052c79883ff5cc9371d166553262e7cb7965
exoutia <jhav365@gmail.com>
2023-12-24 11:17:14 +0530

    adding leetcode.nvim plugin for solving problems.

--- plugins.lua
+++ plugins.lua
@@ -59,7 +59,7 @@ local plugins = {
     { import = "nvcommunity.folds.tailwindfold" },
     {
       "tailwind-fold.nvim",
-       ft = { 'html', 'svelte', 'astro', 'vue', 'typescriptreact', "htmldjango" },
+      ft = { "html", "svelte", "astro", "vue", "typescriptreact", "htmldjango" },
     },
     { import = "nvcommunity.editor.biscuits" },
     { import = "nvcommunity.editor.symbols-outline" },
@@ -386,7 +386,26 @@ local plugins = {
       "nvim-lua/plenary.nvim",
     },
   },
+
+  {
+    "kawre/leetcode.nvim",
+    build = ":TSUpdate html",
+    lazy = false,
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      "nvim-lua/plenary.nvim", -- required by telescope
+      "MunifTanjim/nui.nvim",
+
+      -- optional
+      "nvim-treesitter/nvim-treesitter",
+      "rcarriga/nvim-notify",
+      "nvim-tree/nvim-web-devicons",
+    },
+    opts = {
+      -- configuration goes here
+      lang = "python3"
+    },
+  },
 }
 
 return plugins
-
